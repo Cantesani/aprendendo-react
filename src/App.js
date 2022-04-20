23b5b2bd0b46b45cf7164c41c914e7ae013db33b
Gabriel Cantesani <cantesani@hotmail.com>
2022-04-20 14:42:42 -0300

    aulas Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/user'>User</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+
+        <Route path='/about' element={<About/>} />
+        <Route path='/user' element={<User/>} />
+        <Route path='/' element={<Home/>} />
+
+        
+      </Routes>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
+
+function Home()  {
+return (
+  <div>
+    Exemplo HOME <br/>
+    Este é o Exemplo HOME
+  </div>
+) 
+}
+
+function About() {
+  return(
+    <div>
+      Exemplo ABOUT <br/>
+      Este é o Exemplo ABOUT
+    </div>
+  )
+}
+
+function User () {
+  return (
+    <div>
+      Exemplo USER <br/>
+      Este é o Exemplo USER
+    </div>
+  )
+}
 export default App;
